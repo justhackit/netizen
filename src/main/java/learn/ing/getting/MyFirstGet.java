d904f8a38ab27276aecd544ecac915a2f817fc48
chinna gaadu <ajay.edap@gmail.com>
2014-09-27 21:12:34 +0530

    commit

---
+++ src/main/java/learn/ing/getting/MyFirstGet.java
@@ -0,0 +1,45 @@
+package learn.ing.getting;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.codemahal.netizen.common.GetPostUtils;
+import org.codemahal.netizen.common.ReadUtils;
+
+public class MyFirstGet {
+	static Logger LOG = Logger.getLogger(MyFirstGet.class);
+	URI target;
+	
+	public MyFirstGet(String url) throws ClientProtocolException, IOException, URISyntaxException{
+		target = new URI(url);
+		LOG.info("Received Target -> "+target) ;
+		long startedAt = System.currentTimeMillis();
+		HttpResponse resp = GetPostUtils.getPage(target);
+		LOG.info("Time Taken: "+(System.currentTimeMillis() - startedAt));
+		StringBuilder content = ReadUtils.getContentAsText(resp.getEntity().getContent());
+		
+	}
+	
+	public static void main(String[] args) throws URISyntaxException, ClientProtocolException, IOException {
+		BasicConfigurator.configure();
+		LOG.getRootLogger().setLevel(Level.INFO);
+/*		MyFirstGet mfg = null;
+		if(args.length==0){
+			mfg = new MyFirstGet("http://www.redbus.in");
+		}
+		else mfg = new MyFirstGet(args[0]);
+*/	
+		StringBuilder content = GetPostUtils.getContentAsText("http://175.41.130.244:80/");
+		System.out.println(content);
+		System.out.println("Content length:"+content.length());
+		}
+
+}
---
+++ src/main/java/org/codemahal/main/HitIt.java
@@ -0,0 +1,48 @@
+package org.codemahal.main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import learn.ing.getting.MyFirstGet;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.codemahal.dto.BasicNetizenDTO;
+import org.codemahal.netizen.common.GetPostUtils;
+import org.codemahal.netizen.common.ReadUtils;
+
+public class HitIt {
+	static Logger LOG = Logger.getLogger(HitIt.class);
+	
+	public void knockKnock(BasicNetizenDTO dto) throws ClientProtocolException, IOException{
+		if(dto.getRequest().getMethod().equalsIgnoreCase(HttpGet.METHOD_NAME)){
+			HttpResponse resp = GetPostUtils.getPage(dto.getUri());
+			HttpEntity entity = resp.getEntity();
+			dto.setStatusCode(resp.getStatusLine().getStatusCode());
+			dto.setRespStatusLine(resp.getStatusLine().toString());
+			dto.setContentWeight(ReadUtils.bytesToHigher(entity.getContentLength()));
+			dto.setResponseHeaders(resp.getAllHeaders());
+			dto.setTheContent(ReadUtils.getContentAsText(entity.getContent()));
+		}
+	}
+	public static void main(String[] args) throws Exception {
+		Logger.getLogger("httpclient.wire.header").setLevel(Level.DEBUG);
+		BasicNetizenDTO dto = new BasicNetizenDTO();
+		URIBuilder uriBuilder = null;
+		uriBuilder = new URIBuilder("https://www.facebook.com/login.php?login_attempt=1");
+		uriBuilder.setParameter("email", "ajay.edap@gmail.com");
+		uriBuilder.setParameter("pass","constraint02");
+		dto.setUri(uriBuilder.build());
+		dto.setRequest(new HttpGet());
+		HitIt hit = new HitIt();
+		hit.knockKnock(dto);
+		System.out.println(dto);
+		
+	}
+}
